ab172b02ccc70d83898c32d1cda9cc1efbe6ea1a
chilo <chilo@ACER>
2023-07-26 22:40:12 +0200

    Add project files.

---
+++ Seating Chart/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        btnAdd = New Button()
+        btnDelete = New Button()
+        btnQuit = New Button()
+        txtName = New TextBox()
+        txtSeat = New TextBox()
+        Label3 = New Label()
+        lstWait = New ListView()
+        Label4 = New Label()
+        lvSeatingChart = New ListView()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(42, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Name:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(166, 9)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(32, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Seat:"
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(50, 35)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(110, 32)
+        btnAdd.TabIndex = 2
+        btnAdd.Text = "Add Passenger"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(50, 73)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(110, 34)
+        btnDelete.TabIndex = 3
+        btnDelete.Text = "Delete Passenger"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnQuit
+        ' 
+        btnQuit.Location = New Point(50, 113)
+        btnQuit.Name = "btnQuit"
+        btnQuit.Size = New Size(110, 37)
+        btnQuit.TabIndex = 4
+        btnQuit.Text = "Quit"
+        btnQuit.UseVisualStyleBackColor = True
+        ' 
+        ' txtName
+        ' 
+        txtName.Location = New Point(60, 6)
+        txtName.Name = "txtName"
+        txtName.Size = New Size(100, 23)
+        txtName.TabIndex = 5
+        ' 
+        ' txtSeat
+        ' 
+        txtSeat.Location = New Point(204, 6)
+        txtSeat.Name = "txtSeat"
+        txtSeat.Size = New Size(58, 23)
+        txtSeat.TabIndex = 6
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(60, 153)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(72, 15)
+        Label3.TabIndex = 7
+        Label3.Text = "Waiting List:"
+        ' 
+        ' lstWait
+        ' 
+        lstWait.Location = New Point(50, 171)
+        lstWait.Name = "lstWait"
+        lstWait.Size = New Size(110, 120)
+        lstWait.TabIndex = 8
+        lstWait.UseCompatibleStateImageBehavior = False
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(166, 32)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(149, 15)
+        Label4.TabIndex = 10
+        Label4.Text = "(1A, 1B, 1C, 1D, 2A, ..., 10D)"
+        ' 
+        ' lvSeatingChart
+        ' 
+        lvSeatingChart.Location = New Point(166, 53)
+        lvSeatingChart.Name = "lvSeatingChart"
+        lvSeatingChart.Size = New Size(340, 238)
+        lvSeatingChart.TabIndex = 11
+        lvSeatingChart.UseCompatibleStateImageBehavior = False
+        lvSeatingChart.View = View.Details
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(526, 311)
+        Controls.Add(lvSeatingChart)
+        Controls.Add(Label4)
+        Controls.Add(lstWait)
+        Controls.Add(Label3)
+        Controls.Add(txtSeat)
+        Controls.Add(txtName)
+        Controls.Add(btnQuit)
+        Controls.Add(btnDelete)
+        Controls.Add(btnAdd)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Seating Chart"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtSeat As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lstWait As ListView
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lvSeatingChart As ListView
+End Class
---
+++ Seating Chart/Form1.vb
@@ -0,0 +1,210 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+    ' 2D array for seating chart
+    Dim seatingChart(9, 3) As String
+    ' Array for the waiting list
+    Dim waitingList As New List(Of String)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialise the seating chart with all available seats
+        For row As Integer = 0 To 9
+            For seat As Integer = 0 To 3
+                seatingChart(row, seat) = "."
+            Next
+        Next
+
+        ' Create columns for the ListView
+        lvSeatingChart.Columns.Add("Row", 80)
+        lvSeatingChart.Columns.Add("A", 60)
+        lvSeatingChart.Columns.Add("B", 80)
+        lvSeatingChart.Columns.Add("C", 60)
+        lvSeatingChart.Columns.Add("D", 60)
+
+        ' Load reservations from file
+        LoadReservationsFromFile()
+
+        ' Display initial seating chart on form load
+        UpdateSeatingChartDisplay()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim name As String = txtName.Text.Trim()
+        Dim seatInput As String = txtSeat.Text.Trim().ToUpper() ' Convert to uppercase for case-insensitive comparison
+
+        'Check if the name and seat input are provided
+        If name = "" Or seatInput = "" Then
+            MessageBox.Show("Please provide passenger name and seat number.")
+            Return
+        End If
+
+        'Check if the seat input is valid
+        If Not IsValidSeatInput(seatInput) Then
+            MessageBox.Show("Invalid seat number format. Please enter a valid seat number (e.g., 1A, 2B, 10D).")
+            Return
+        End If
+
+        Dim row As Integer
+        Dim seatIndex As Integer
+
+        'Check number of characters for seat number
+        If seatInput.Length = 2 Then
+            ' Seat numbers from 1A to 9D 
+            row = CInt(seatInput.Substring(0, 1)) - 1
+            seatIndex = Asc(seatInput.Substring(1, 1)) - Asc("A")
+        ElseIf seatInput.Length = 3 Then
+            ' Seat numbers for rows 10A to 10D
+            row = CInt(seatInput.Substring(0, 2)) - 1
+            seatIndex = Asc(seatInput.Substring(2, 1)) - Asc("A")
+        Else
+            ' Invalid seat number format
+            MessageBox.Show("Invalid seat number format. Please enter a valid seat number (e.g., 1A, 2B, 10D).")
+            Return
+        End If
+
+        'Check if the seat is available
+        If seatingChart(row, seatIndex) = "." Then
+            seatingChart(row, seatIndex) = name
+            UpdateSeatingChartDisplay()
+        Else
+            'If the seat is occupied, add the passenger to the waiting list
+            waitingList.Add(name)
+            MessageBox.Show("Seat already taken. Passenger added to the waiting list.")
+            lstWait.Items.Add(name)
+        End If
+
+        'Clear the name and seat input textboxes
+        txtName.Clear()
+        txtSeat.Clear()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim name As String = txtName.Text.Trim().ToUpper() ' Convert to uppercase for case-insensitive comparison
+
+        ' Check if the name is provided
+        If name = "" Then
+            MessageBox.Show("Please enter the passenger's name.")
+            Return
+        End If
+
+        ' Search the seating chart for the passenger's name and delete it
+        For row As Integer = 0 To 9
+            For seat As Integer = 0 To 3
+                If String.Equals(seatingChart(row, seat), name, StringComparison.OrdinalIgnoreCase) Then
+                    seatingChart(row, seat) = "."
+                    UpdateSeatingChartDisplay()
+
+                    ' If the waiting list is not empty, assign the next passenger from the waiting list
+                    If waitingList.Count > 0 Then
+                        Dim waitingPassenger As String = waitingList(0)
+                        waitingList.RemoveAt(0)
+                        seatingChart(row, seat) = waitingPassenger
+                        UpdateSeatingChartDisplay()
+                        lstWait.Items.RemoveAt(0)
+                    End If
+
+                    ' Clear the name textbox
+                    txtName.Clear()
+                    Return
+                End If
+            Next
+        Next
+
+        ' If the passenger's name is not found in the seating chart or waiting list
+        MessageBox.Show("Passenger not found on the flight or waiting list.")
+        txtName.Clear()
+    End Sub
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        Me.Close()
+    End Sub
+
+    Private Function IsPlaneFull() As Boolean
+        For row As Integer = 0 To 9
+            For seat As Integer = 0 To 3
+                If seatingChart(row, seat) = "." Then
+                    Return False
+                End If
+            Next
+        Next
+        Return True
+    End Function
+
+    'Get the first available seat in the seating chart
+    Private Function GetAvailableSeat() As String
+        For row As Integer = 0 To 9
+            For seat As Integer = 0 To 3
+                If seatingChart(row, seat) = "." Then
+                    Return (row + 1) & Chr(seat + 65)
+                End If
+            Next
+        Next
+        Return Nothing
+    End Function
+
+    Private Function IsValidSeatInput(seatInput As String) As Boolean
+        Dim validSeatRegex As New Regex("^([1-9]|10)[A-D]$", RegexOptions.IgnoreCase)
+        Return validSeatRegex.IsMatch(seatInput)
+    End Function
+
+    'Update the seating chart display in the ListView
+    Private Sub UpdateSeatingChartDisplay()
+        lvSeatingChart.Items.Clear()
+        For row As Integer = 0 To 9
+            Dim rowDisplay As New List(Of String)()
+            rowDisplay.Add((row + 1).ToString()) ' Add the row number as the first column
+
+            For seat As Integer = 0 To 3
+                If seatingChart(row, seat) = "." Then
+                    rowDisplay.Add(".")
+                Else
+                    rowDisplay.Add("X")
+                End If
+            Next
+
+            Dim listViewItem As New ListViewItem(rowDisplay.ToArray())
+            lvSeatingChart.Items.Add(listViewItem)
+        Next
+    End Sub
+
+    Private Sub SaveReservationsToFile()
+        Dim filePath As String = "reservations.txt"
+        Using writer As New System.IO.StreamWriter(filePath)
+            For row As Integer = 0 To 9
+                For seat As Integer = 0 To 3
+                    writer.WriteLine(seatingChart(row, seat))
+                Next
+            Next
+
+            For Each passenger As String In waitingList
+                writer.WriteLine(passenger)
+            Next
+        End Using
+    End Sub
+
+    Private Sub LoadReservationsFromFile()
+        Dim filePath As String = "reservations.txt"
+        If System.IO.File.Exists(filePath) Then
+            Dim lines As String() = System.IO.File.ReadAllLines(filePath)
+            Dim index As Integer = 0
+            For row As Integer = 0 To 9
+                For seat As Integer = 0 To 3
+                    seatingChart(row, seat) = lines(index)
+                    index += 1
+                Next
+            Next
+
+            waitingList.Clear()
+            While index < lines.Length
+                waitingList.Add(lines(index))
+                index += 1
+            End While
+
+            UpdateSeatingChartDisplay()
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        SaveReservationsToFile()
+    End Sub
+End Class
